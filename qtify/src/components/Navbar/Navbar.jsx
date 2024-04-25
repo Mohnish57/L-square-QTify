dd20dd9a0a03f8c9236424ef888a7a0ce3cee7f5
monu <sawlanimohnish@gmail.com>
2024-04-25 14:18:50 +0530

    dependencies issue resolved

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import Logo from "../Logo/Logo";
 import Search from '../Search/Search';
 import Feedback from '../Feedback/Feedback';
 import styles from './Navbar.module.css'
+import { Box } from '@mui/material';
 const Navbar = () => {
   return (
     <>
